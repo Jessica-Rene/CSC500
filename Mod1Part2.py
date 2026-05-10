588ad6b98d23180daacdddaf6b419fa9633e9e0a
Jessica-Rene <j.r.reyes1109@gmail.com>
2026-05-10 15:16:44 -0600

    Resolve merge conflict and update Module1 files

--- Mod1Part2.py
+++ Mod1Part2.py
@@ -3,8 +3,10 @@
 num1 = int(input("Enter first number: "))
 num2 = int(input("Enter second number: "))
 
+# Perform multiplication and division
 multiplication = num1 * num2
 division = num1 / num2
 
+# Display the results
 print("Multiplication:", multiplication)
 print("Division:", division)
